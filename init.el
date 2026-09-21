003aa941d117bf5791023098186f53bdd7dd0f14
daijinguo <jinguo.dai@nio.com>
2026-09-21 18:31:44 +0800

    修复 macos 27 无法使用 emacs31.1 问题

--- init.el
+++ init.el
@@ -1,5 +1,10 @@
 ;;; -*- lexical-binding: t no-byte-compile: t -*-
 
+;; macOS 27 (Darwin 27) 上 Emacs 31.1 内嵌的 libgccjit 检测部署目标时算出了
+;; 无效版本 18.0，导致传给 clang driver 的 -mmacosx-version-min=18.0 被拒绝
+;; （native-ice: error invoking gcc driver）。提前设置部署目标即可修复。
+(setenv "MACOSX_DEPLOYMENT_TARGET" "27.0")
+
 ;; load the path for self config files
 (let ((dir (locate-user-emacs-file "lisp")))
   (add-to-list 'load-path (file-name-as-directory dir)))
